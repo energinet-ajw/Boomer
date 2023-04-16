3d3a8614b538da88545471b74b142ddc8c6a59d0
Alexander <122119488+AHdevelopment@users.noreply.github.com>
2023-04-16 15:11:46 +0200

    Create MouseCreatedDomainEventHandler.cs

---
+++ PetStore.Application/Cat/EventHandlers/MouseCreatedDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using PetStore.Application.Base;
+using PetStore.Domain.CatAggregate;
+
+namespace PetStore.Application.Cat.EventHandlers;
+
+public class CatCreatedDomainEventHandler : IDomainEventHandler<CatCreatedDomainEvent>
+{
+   
+    public async Task Handle(CatCreatedDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        await Console.Out.WriteLineAsync($"CatCreatedDomainEventHandler: New cat created (Id: {domainEvent.CatId}).").ConfigureAwait(false);
+    }
+}
